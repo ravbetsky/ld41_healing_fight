f02f2bc7878fe4fe967eee9dd679b63de8270854
ravbetsky <ravbetsky@gmail.com>
2018-04-21 14:38:07 +0400

    remove some files

--- src/main.js
+++ src/main.js
@@ -1,30 +1,22 @@
 import 'phaser';
 import BootScene from './BootScene';
-import MarioBrosScene from './MarioBrosScene';
-import TitleScene from './TitleScene';
 
-
-let config = {
-    type: Phaser.WEBGL,
-    parent: 'content',
-    width: 400,
-    height: 240,
-    scaleMode: 0, //Phaser.ScaleManager.EXACT_FIT,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 800 },
-            debug: false
-        }
-    },
-    scene: [
-        BootScene,
-        TitleScene,
-        MarioBrosScene,
-    ]
+const config = {
+  type: Phaser.WEBGL,
+  parent: 'content',
+  width: 400,
+  height: 240,
+  scaleMode: 0, //Phaser.ScaleManager.EXACT_FIT,
+  physics: {
+    default: 'arcade',
+      arcade: {
+        gravity: { y: 800 },
+        debug: false
+      }
+  },
+  scene: [
+    BootScene,
+  ]
 };
 
-let game = new Phaser.Game(config);
-
-/*
-https://codepen.io/samme/pen/JMVBeV*/
+const game = new Phaser.Game(config);
